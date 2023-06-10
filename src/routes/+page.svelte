20d0ee7a5263632378c3f754d347cc413bc1eaf6
Prabhu Kiran Konda <prabhukiran8790@protonmail.com>
2023-06-10 12:15:21 +0530

    bug fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,13 @@
 <script>
-// @ts-nocheck
-
-	// @ts-ignore
 	import Header from '../Components/Header.svelte';
-	// @ts-ignore
-	import Results from '../components/Results.svelte';
+	import Results from '../Components/Results.svelte';
 	import { storeInputValue } from '../store.js';
-
 
 	let inputValue;
 
 	storeInputValue.subscribe((value) => {
 		inputValue = value;
 	});
-
 </script>
 
 <div class="overflow-x-hidden">
