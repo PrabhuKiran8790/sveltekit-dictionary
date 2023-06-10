a9073320f0c7c3023e2301d2c95831c25cfa082c
Prabhu Kiran Konda <prabhukiran8790@protonmail.com>
2023-06-10 13:21:26 +0530

    bug fix

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -16,39 +16,41 @@
 	}
 </script>
 
-<div class="card p-4 mx-24 my-4 variant-soft-surface">
-	<div class="container mx-auto py-8">
-		<h1 class="h1 text-center">
-			<span
-				class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
-				>Dictionary</span
-			>
-		</h1>
-		<p class="text-center mt-1 mb-10">A Svelte Project</p>
-		<div class="flex items-center justify-center">
-			<input
-				bind:value={inputValue}
-				on:keydown={(e) => {
-					if (e.key === 'Enter' && inputValue.length > 0) {
-						handleSubmit();
-					}
-				}}
-				type="text"
-				placeholder="Search"
-				class="text-black font-bold w-64 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-			/>
-			<button
-				on:click={handleSubmit}
-				class="ml-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-				>Submit</button
-			>
+<div class="card variant-soft-surface flex justify-center mt-5 mx-3 lg:mx-24">
+	<div class="w-4/5">
+		<div class="container mx-auto py-8">
+			<h1 class="h1 text-center">
+				<span
+					class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+					>Dictionary</span
+				>
+			</h1>
+			<p class="text-center mt-1 mb-10">A Svelte Project</p>
+			<div class="flex items-center justify-center">
+				<input
+					bind:value={inputValue}
+					on:keydown={(e) => {
+						if (e.key === 'Enter' && inputValue.length > 0) {
+							handleSubmit();
+						}
+					}}
+					type="text"
+					placeholder="Search"
+					class="text-black font-bold w-64 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+				/>
+				<button
+					on:click={handleSubmit}
+					class="ml-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+					>Submit</button
+				>
+			</div>
 		</div>
+		{#if displayValue}
+			<div class="select-none">
+				<p class="text-center">
+					Results for: <span class="text-lg font-bold text-teal-300 underline">{displayValue}</span>
+				</p>
+			</div>
+		{/if}
 	</div>
-	{#if displayValue}
-		<div class="select-none">
-			<p class="text-center">
-				Results for: <span class="text-lg font-bold text-teal-300 underline">{displayValue}</span>
-			</p>
-		</div>
-	{/if}
 </div>
